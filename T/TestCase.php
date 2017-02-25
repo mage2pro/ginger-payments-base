e378895e9db9ecd4ae07ca900946f0e2f1208c0b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-25 22:25:23 +0300

    no message

--- T/TestCase.php
+++ T/TestCase.php
@@ -1,4 +1,11 @@
 <?php
 // 2017-02-25
 namespace Df\GingerPaymentsBase\T;
-abstract class TestCase extends \Df\Core\TestCase {}
+use \GingerPayments\Payment\Ginger as API;
+abstract class TestCase extends \Df\Core\TestCase {
+	/**
+	 * 2017-02-15
+	 * @return API
+	 */
+	final protected function api() {return null;}
+}
