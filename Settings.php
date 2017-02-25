44f0ae19ef1c591c79854f2a98f012387860df1c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-25 22:29:23 +0300

    no message

---
+++ Settings.php
@@ -0,0 +1,9 @@
+<?php
+namespace Df\GingerPaymentsBase;
+/**
+ * 2017-02-25
+ * @method static Settings s()
+ * @see \Dfe\GingerPayments\Settings
+ * @see \Dfe\KassaCompleet\Settings
+ */
+abstract class Settings extends \Df\Payment\Settings {}
