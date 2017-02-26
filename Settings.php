e257790a665cab35bf7a144b5171d36de283e4a0
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-26 15:00:27 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -12,7 +12,7 @@ use Magento\Store\Model\Store;
  * @see \Dfe\GingerPayments\Settings
  * @see \Dfe\KassaCompleet\Settings
  */
-abstract class Settings extends \Df\Payment\Settings {
+abstract class Settings extends \Df\Payment\Settings implements \GingerPayments\Payment\IProduct {
 	/**
 	 * 2017-02-26
 	 * @see \Dfe\GingerPayments\Settings::apiDomain()
@@ -42,15 +42,23 @@ abstract class Settings extends \Df\Payment\Settings {
 	final public function api($test = null, $s = null) {return dfc($this, function($test, $s) {
 		/** @var string $apiKey */
 		$apiKey = $this->privateKey($s);
-		Guard::uuid(G::apiKeyToUuid($apiKey), "API key is invalid: «{$apiKey}».");
-		/** @var string $product */
-		$product = $this->product();
-        return new API(new HttpClient([
+		Guard::uuid(self::apiKeyToUuid($apiKey), "API key is invalid: «{$apiKey}».");
+        return new API($this, new HttpClient([
 			'auth' => [$apiKey, '']
 			,'base_url' => "https://api.{$this->apiDomain()}/v1/"
 			,'headers' => df_headers(['User-Agent' => df_cc_s(
-				'Mage2.PRO', $product, df_package_version($this)
+				'Mage2.PRO', $this->product(), df_package_version($this)
 			)])
-		]), $product);
+		]));
 	}, [!is_null($test) ? $test : $this->test(), $s]);}
+
+    /**
+     * 2017-02-26
+	 * @used-by api()
+     * @param string $apiKey
+     * @return string UUID
+     */
+    private static function apiKeyToUuid($apiKey) {return
+		preg_replace('/(\w{8})(\w{4})(\w{4})(\w{4})(\w{12})/', '$1-$2-$3-$4-$5', $apiKey)
+	;}
 }
