520b7beabeb4ab54211e327cbb1be3f845f8b80e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-27 17:51:49 +0300

    no message

--- T/CreateOrder.php
+++ T/CreateOrder.php
@@ -19,37 +19,39 @@ abstract class CreateOrder extends TestCase {
 	 * 2017-02-27
 	 */
 	final function t01_success() {
-		$this->api()->createOrder(
+		$this->api()->postOrder([
 			// 2017-02-27
 			// The amount in cents
-			2500
+			'amount' => 2500
 			// 2017-02-27
 			// The currency
-			,'EUR'
-			// 2017-02-27
-			// The payment method
-			,$this->method()
-			// 2017-02-27
-			// Extra details required for this payment method
-			,$this->params()
+			,'currency' => 'EUR'
 			// 2017-02-27
 			// «A description (optional)»
 			// I did not find any limitations on it.
-			,'Пример <b>описания</b> <a href="https:://mage2.pro">заказа</a> د ويکيپېډيا، وړیا پوهنغونډ له خوا 묘사(描寫)는 사물의'
+			,'description' => 'Пример <b>описания</b> <a href="https:://mage2.pro">заказа</a> د ويکيپېډيا، وړیا پوهنغونډ له خوا 묘사(描寫)는 사물의'
+			// 2017-02-27
+			// The expiration period in ISO 8601 format (optional)
+			// [Ginger Payments] The «expiration_period» parameter for POST /v1/orders/ is undocumented: https://mage2.pro/t/3388
+			,'expiration_period' => 'PT15M'
 			// 2017-02-27
 			// Your identifier for the order (optional)
-			,'order-234192'
+			,'merchant_order_id' => 'order-234192'
 			// 2017-02-27
 			// «The return URL (optional)».
 			// This parameter is required for iDEAL:
 			// https://s3-eu-west-1.amazonaws.com/wl1-apidocs/api.kassacompleet.nl/index.html#creating-an-ideal-order
 			// https://www.gingerpayments.com/docs#creating-an-ideal-order
-			,'https://mage2.pro'
-			// 2017-02-27
-			// The expiration period in ISO 8601 format (optional)
-			// [Ginger Payments] The «expiration_period» parameter for POST /v1/orders/ is undocumented: https://mage2.pro/t/3388
-			,'PT15M'
-		);
+			,'return_url' => 'https://mage2.pro'
+			,'transactions' => [[
+				// 2017-02-27
+				// The payment method
+				'payment_method' => $this->method()
+				// 2017-02-27
+				// Extra details required for this payment method
+				,'payment_method_details' => $this->params()
+			]]
+		]);
 		echo $this->api()->lastResponse();
 	}
 
