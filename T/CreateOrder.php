2c15bb8253b841c3e8f872e7d4071c348bc23003
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-28 13:54:17 +0300

    no message

--- T/CreateOrder.php
+++ T/CreateOrder.php
@@ -26,6 +26,13 @@ abstract class CreateOrder extends TestCase {
 			// 2017-02-27
 			// The amount in cents
 			'amount' => 2500
+			// 2017-02-28
+			// [Ginger Payments] A documentation for the «customer» parameter
+			// in the «POST /v1/orders/» request: https://mage2.pro/t/3394
+			// The official Ginger Payments and Kassa Compleet extensions for Magento 1.x
+			// pass the same customer data to «POST /v1/orders/» besides the «customer/locale» format:
+			// https://mage2.pro/t/3445
+			,'customer' => $this->customer()
 			// 2017-02-27
 			// The currency
 			,'currency' => 'EUR'
@@ -65,4 +72,55 @@ abstract class CreateOrder extends TestCase {
 	 * @return string
 	 */
 	protected function params() {return [];}
+
+	/**
+	 * 2017-02-28
+	 * The official Ginger Payments and Kassa Compleet extensions for Magento 1.x
+	 * pass the same customer data to «POST /v1/orders/» besides the «customer/locale» format:
+	 * https://mage2.pro/t/3445
+	 * @used-by t01_success()
+	 * @return array(string => string)
+	 */
+	private function customer() {return [
+		// 2017-02-28
+		// Test addresses for some countries: https://mage2.pro/t/2555
+		'address' => 'Amsterdam Rusland 17'
+		,'address_type' => 'billing'
+		// 2017-02-28
+		// [Kassa Compleet] What is the right format of a customer's birth date in a «POST /v1/orders/» request?
+		// https://mage2.pro/t/3448
+		// [Kassa Compleet] The «customer/birth_date» property is absent in the JSON Schema
+		// of a «POST /v1/orders/» request, but is passed by the official extension for Magento 1.x:
+		// https://mage2.pro/t/3447
+		// [Ginger Payments] The «customer/birth_date» property is absent in the JSON Schema
+		// of a «POST /v1/orders/» request, but is passed by the official extension for Magento 1.x:
+		// https://mage2.pro/t/3446
+		,'birth_date' => '1982-07-08 00:00:00'
+		,'country' => 'NL'
+		,'email_address' => 'admin@mage2.pro'
+		,'first_name' => 'Dmitry'
+		,'forwarded_ip' => '5.9.188.84'
+		// "male", "female", "other", null
+		,'gender' => 'male'
+		,'housenumber' => '17'
+		,'ip_address' => '5.9.188.84'
+		,'last_name' => 'Fedyuk'
+		// 2017-02-28
+		// [Kassa Compleet] What is the right format of a customer's locale in a «POST /v1/orders/» request?
+		// https://mage2.pro/t/3444
+		// [Ginger Payments] The «customer/locale» property is absent in the JSON Schema
+		// of a «POST /v1/orders/» request, but is passed by the official extension for Magento 1.x:
+		// https://mage2.pro/t/3443
+		// The official Ginger Payments and Kassa Compleet extensions for Magento 1.x
+		// pass the same customer data to «POST /v1/orders/» besides the «customer/locale» format:
+		// https://mage2.pro/t/3445
+		,'locale' => 'nl_NL'
+		,'merchant_customer_id' => '123'
+		,'phone_numbers' => ['+31 20 623 1231']
+		// 2017-02-28
+		// Test addresses for some countries: https://mage2.pro/t/2555
+		,'postal_code' => '1012'
+		,'referrer' => 'https://mage2.pro'
+		,'user_agent' => 'Mage2.PRO'
+	];}
 }
