a3ec16b95d194a2dda8f8c750954dd26909bf910
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-01 22:29:01 +0300

    no message

--- Source/Option.php
+++ Source/Option.php
@@ -3,6 +3,8 @@ namespace Df\GingerPaymentsBase\Source;
 use GingerPayments\Payment\Order\Transaction\PaymentMethod as M;
 /**
  * 2017-03-01
+ * [Ginger Payments] Available payment options: https://mage2.pro/t/3463
+ * [Kassa Compleet] Available payment options: https://mage2.pro/t/3248
  * @see \Dfe\GingerPayments\Source\Option
  * @see \Dfe\KassaCompleet\Source\Option
  * @method static Option s()
@@ -25,6 +27,16 @@ abstract class Option extends \Df\Config\SourceT {
 	 * @return array(string => string)
 	 */
 	final protected function map() {return [
-		M::IDEAL => 'iDEAL'
+		M::BANK_CARD => 'Bank Card'
+		,self::BANK_TRANSFER => 'Bank Transfer'
+		,M::IDEAL => 'iDEAL'
 	] + $this->mapExtra();}
+
+	/**
+	 * 2017-03-01
+	 * @used-by map()
+	 * @see \GingerPayments\Payment\Order\Transaction\PaymentMethod::BANK_TRANSFER_ING
+	 * @see \GingerPayments\Payment\Order\Transaction\PaymentMethod::BANK_TRANSFER_GINGER
+	 */
+	const BANK_TRANSFER = 'bank-transfer';
 }
