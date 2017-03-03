1265876560ee06ef828c0c838b7c290a06dd8969
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-03 12:26:52 +0300

    no message

---
+++ ConfigProvider.php
@@ -0,0 +1,9 @@
+<?php
+namespace Df\GingerPaymentsBase;
+/**
+ * 2017-03-03
+ * @see \Dfe\GingerPayments\ConfigProvider
+ * @see \Dfe\KassaCompleet\ConfigProvider
+ * @method Settings s()
+ */
+abstract class ConfigProvider extends \Df\Payment\ConfigProvider {}
