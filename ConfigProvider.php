c455b4b12fa61c7ed0ef1c2099b25d6ba2821bf2
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-03 16:47:56 +0300

    no message

--- ConfigProvider.php
+++ ConfigProvider.php
@@ -4,11 +4,15 @@ namespace Df\GingerPaymentsBase;
  * 2017-03-03
  * The class is not abstract,
  * because it is used as a base for virtual types in the following modules:
+ *
  * 1) GingerPayments:
  * https://github.com/mage2pro/ginger-payments/blob/0.0.8/etc/frontend/di.xml?ts=4#L13-L15
  * https://github.com/mage2pro/ginger-payments/blob/0.0.8/etc/frontend/di.xml?ts=4#L9
  *
  * 2) KassaCompleet:
+ * https://github.com/mage2pro/kassa-compleet/tree/0.1.3/etc/frontend/di.xml?ts=4#L13-L15
+ * https://github.com/mage2pro/kassa-compleet/tree/0.1.3/etc/frontend/di.xml?ts=4#L9
+ *
  * @method Settings s()
  */
 class ConfigProvider extends \Df\Payment\ConfigProvider {}
