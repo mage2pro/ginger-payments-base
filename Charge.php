b54e0abf6f76a57c0d94cf858004fa4b116e8ef5
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-06 01:57:48 +0300

    no message

--- Charge.php
+++ Charge.php
@@ -15,7 +15,25 @@ final class Charge extends \Df\Payment\Charge {
 		// 2017-02-27
 		// «Order amount (including VAT)».
 		'amount' => $this->amountF()
+		// 2017-02-28
+		// The «client» property is present only in the Kassa Compleet's JSON schema,
+		// but Ginger Payments does not fail if it is specified (just silently ignores it).
+		,'client' => $this->pClient()
 	];}
+
+	/**
+	 * 2017-03-06
+	 * @used-by pCharge()
+	 * @return array(string => mixed)
+	 */
+	private function pClient() {return [
+		'platform_name' => "Mage2.PRO «{$this->m()->titleB()}» extension for Magento 2 (https://mage2.pro)"
+		,'platform_version' => df_package_version($this->m())
+		// 2017-02-28
+		// It will be rewritten to «Ginger-Python-ApiClient/0.8.1 Requests/2.11.1 Python/2.7.10».
+		,'user_agent' => 'Mage2.PRO (https://mage2.pro)'
+	];}
+
 
 	/**
 	 * 2017-03-06
