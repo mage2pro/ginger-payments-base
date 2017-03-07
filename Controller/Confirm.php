cc91580e05145cbc01c8b08aeae308d63699ac01
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-07 11:03:35 +0300

    no message

---
+++ Controller/Confirm.php
@@ -0,0 +1,8 @@
+<?php
+namespace Df\GingerPaymentsBase\Controller;
+/**
+ * 2017-03-07
+ * @see \Dfe\GingerPayments\Controller\Confirm\Index
+ * @see \Dfe\KassaCompleet\Controller\Confirm\Index
+ */
+abstract class Confirm extends \Df\Payment\Action\Webhook {}
---
+++ Controller/CustomerReturn.php
@@ -0,0 +1,8 @@
+<?php
+namespace Df\GingerPaymentsBase\Controller;
+/**
+ * 2017-03-07
+ * @see \Dfe\GingerPayments\Controller\CustomerReturn\Index
+ * @see \Dfe\KassaCompleet\Controller\CustomerReturn\Index
+ */
+abstract class CustomerReturn extends \Df\Payment\Action\CustomerReturn {}
