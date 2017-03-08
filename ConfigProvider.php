e2fbd99f0236c77e6b347e9f13cfff37174349f2
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-09 02:17:52 +0300

    no message

--- ConfigProvider.php
+++ ConfigProvider.php
@@ -30,6 +30,8 @@ final class ConfigProvider extends \Df\Payment\ConfigProvider {
 		// @used-by Df_GingerPaymentsBase/main::banks()
 	  	// https://github.com/mage2pro/ginger-payments-base/blob/0.2.3/view/frontend/web/main.js?ts=4#L7-L21
 		'banks' => $this->m()->api()->idealBanks()
+		// 2017-03-09
+		// @used-by Df_GingerPaymentsBase/bankTransfer
 		,'bankTransferCheckoutMessage' => $s->v('bankTransferCheckoutMessage')
 		// 2017-03-04
 		// @used-by Df_Payments/withOptions::options()
