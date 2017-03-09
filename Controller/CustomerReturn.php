241deb7cc25868f4525817f04edf3758f417e745
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-09 07:19:43 +0300

    no message

--- Controller/CustomerReturn.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Df\GingerPaymentsBase\Controller;
-/**
- * 2017-03-07
- * @see \Dfe\GingerPayments\Controller\CustomerReturn\Index
- * @see \Dfe\KassaCompleet\Controller\CustomerReturn\Index
- */
-abstract class CustomerReturn extends \Df\Payment\Action\CustomerReturn {}
---
+++ Webhook.php
@@ -0,0 +1,34 @@
+<?php
+namespace Df\GingerPaymentsBase;
+/**
+ * 2017-03-09
+ * https://s3-eu-west-1.amazonaws.com/wl1-apidocs/api.kassacompleet.nl/index.html#webhooks
+ * @see \Dfe\GingerPayments\Webhook
+ * @see \Dfe\KassaCompleet\Webhook
+ */
+class Webhook extends \Df\PaypalClone\Confirmation {
+	/**
+	 * 2017-03-09
+	 * @override
+	 * @see \Df\Payment\Webhook::config()
+	 * @used-by \Df\Payment\Webhook::configCached()
+	 * @return array(string => mixed)
+	 */
+	final protected function config() {return [
+		self::$externalIdKey => 'STUB'
+		,self::$needCapture => true
+		,self::$readableStatusKey => 'STUB'
+		,self::$signatureKey => 'STUB'
+		,self::$statusExpected => 1
+		,self::$statusKey => 'STUB'
+	];}
+
+	/**
+	 * 2017-03-09
+	 * @override
+	 * @see \Df\Payment\Webhook::parentIdRawKey()
+	 * @used-by \Df\Payment\Webhook::parentIdRaw()
+	 * @return string
+	 */
+	final protected function parentIdRawKey() {return 'order_id';}
+}
