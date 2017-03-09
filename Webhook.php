29174342faf24f61dbf87c124bce41be44d73e73
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-09 16:42:04 +0300

    no message

--- Webhook.php
+++ Webhook.php
@@ -3,6 +3,12 @@ namespace Df\GingerPaymentsBase;
 /**
  * 2017-03-09
  * https://s3-eu-west-1.amazonaws.com/wl1-apidocs/api.kassacompleet.nl/index.html#webhooks
+ * An example of the notification:
+ *	{
+ *		"event": "status_changed",
+ *		"order_id": "0125e02b-557c-4fb5-956a-d22662d71ad9",
+ *		"project_id": "1ef558ed-d77d-470d-b43b-c0f4a131bcef"
+ *	}
  * @see \Dfe\GingerPayments\Webhook
  * @see \Dfe\KassaCompleet\Webhook
  */
@@ -18,7 +24,6 @@ class Webhook extends \Df\PaypalClone\Confirmation {
 		self::$externalIdKey => 'STUB'
 		,self::$needCapture => true
 		,self::$readableStatusKey => 'STUB'
-		,self::$signatureKey => 'STUB'
 		,self::$statusExpected => 1
 		,self::$statusKey => 'STUB'
 	];}
@@ -35,6 +40,7 @@ class Webhook extends \Df\PaypalClone\Confirmation {
 	/**
 	 * 2017-03-09
 	 * Kassa Compleet и Ginger Payments не подписывают оповещения.
+	 * @todo Мы должны валидировать оповещение посредством запроса к API.
 	 * @override
 	 * @see \Df\PaypalClone\Webhook::validate()
 	 * @used-by \Df\Payment\Webhook::handle()
