24b60a169f54fd4e867c5d0b63d9c30922f5f5b8
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-19 18:13:25 +0300

    0.6.1

--- Controller/Confirm.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Df\GingerPaymentsBase\Controller;
-/**
- * 2017-03-07
- * @see \Dfe\GingerPayments\Controller\Confirm\Index
- * @see \Dfe\KassaCompleet\Controller\Confirm\Index
- */
-abstract class Confirm extends \Df\Payment\W\Action {}
