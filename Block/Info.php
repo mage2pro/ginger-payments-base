04ab9d8c87279b1d0c2df849e68f2fb36f354b64
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-23 17:30:06 +0300

    no message

--- Block/Info.php
+++ Block/Info.php
@@ -1,16 +1,13 @@
 <?php
 namespace Df\GingerPaymentsBase\Block;
-/**
- * 2017-03-09
- * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
- * @method string|null responseF(string $k = null)
- */
-class Info extends \Df\PaypalClone\BlockInfo {
+// 2017-03-09
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+final class Info extends \Df\Payment\Block\Info {
 	/**
 	 * 2017-03-09
 	 * @override
 	 * @see \Df\Payment\Block\Info::prepare()
 	 * @used-by \Df\Payment\Block\Info::_prepareSpecificInformation()
 	 */
-	final protected function prepare() {}
+	protected function prepare() {}
 }
