43ff79e3946fa36b424def015183884d3a6e5941
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-12 12:24:49 +0300

    1.1.3

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -21,13 +21,12 @@ define([
 		bank: ''
 	},
 	/**
-	 * 2016-08-08
-	 * 2017-03-01
-	 * Задаёт набор передаваемых на сервер при нажатии кнопки «Place Order» данных.
+	 * 2017-03-01 Задаёт набор передаваемых на сервер при нажатии кнопки «Place Order» данных.
 	 * @override
-	 * @see mage2pro/core/Payment/view/frontend/web/mixin.js::dfData()
-	 * @used-by mage2pro/core/Payment/view/frontend/web/mixin.js::getData()
-	 * https://github.com/mage2pro/core/blob/2.0.21/Payment/view/frontend/web/mixin.js?ts=4#L208-L225
+	 * @see Df_Payment/withOptions::dfData()
+	 * https://github.com/mage2pro/core/blob/2.8.4/Payment/view/frontend/web/withOptions.js#L19-L33
+	 * @used-by Df_Payment/mixin::getData()
+	 * https://github.com/mage2pro/core/blob/2.8.4/Payment/view/frontend/web/mixin.js#L224
 	 * @see \Df\GingerPaymentsBase\Method::$II_BANK
 	 * https://github.com/mage2pro/ginger-payments-base/blob/0.2.2/Method.php?ts=4#L68
 	 * @see \Df\GingerPaymentsBase\Method::$II_OPTION
