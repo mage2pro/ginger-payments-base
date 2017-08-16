bd8d8195c19e171ebe467587f0373cfd22e9492d
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-16 15:22:56 +0300

    no message

--- W/Reader.php
+++ W/Reader.php
@@ -16,7 +16,7 @@ final class Reader extends \Df\Payment\W\Reader\Json {
 	 * 2017-03-26
 	 * @override
 	 * @see \Df\Payment\W\Reader::kt()
-	 * @used-by \Df\Payment\W\Reader::t()
+	 * @used-by \Df\Payment\W\Reader::tRaw()
 	 * @return string
 	 */
 	protected function kt() {return 'event';}
