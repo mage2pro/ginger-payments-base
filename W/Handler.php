dddee528336958f30e0c32da8362e0fb25705ff3
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-16 09:14:43 +0300

    1.1.6

--- W/Handler.php
+++ W/Handler.php
@@ -2,15 +2,18 @@
 namespace Df\GingerPaymentsBase\W;
 use Df\GingerPaymentsBase\Method as M;
 use Df\Payment\W\Strategy\ConfirmPending;
-// 2017-03-09
-/** @method M m() */
-final class Handler extends \Df\StripeClone\W\Handler {
+/**
+ * 2017-03-09
+ * @method Event e()
+ * @method M m()
+ */
+final class Handler extends \Df\Payment\W\Handler {
 	/**
 	 * 2017-03-26
 	 * https://s3-eu-west-1.amazonaws.com/wl1-apidocs/api.kassacompleet.nl/index.html#possible-returned-order-statuses
 	 * @override
-	 * @see \Df\StripeClone\W\Handler::strategyC()
-	 * @used-by \Df\StripeClone\W\Handler::_handle()
+	 * @see \Df\Payment\W\Handler::strategyC()
+	 * @used-by \Df\Payment\W\Handler::handle()
 	 * @return string|null
 	 */
 	protected function strategyC() {return
