8c32a1eb4b7b3092e0c1eb84546961f32d27e2d1
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-28 19:39:36 +0600

    no message

--- ConfigProvider.php
+++ ConfigProvider.php
@@ -40,7 +40,12 @@ final class ConfigProvider extends \Df\Payment\ConfigProvider implements IOption
 		// @used-by Df_GingerPaymentsBase/main::banks()
 	  	// https://github.com/mage2pro/ginger-payments-base/blob/0.2.3/view/frontend/web/main.js?ts=4#L7-L21
 		'banks' => $this->m()->api()->idealBanks()
-		// 2017-03-09 @used-by Df_GingerPaymentsBase/bankTransfer
+		  /**
+		   * 2017-03-09 @used-by Df_GingerPaymentsBase/bankTransfer
+		   * 2019-09-28
+		   * @see \Dfe\ACH\ConfigProvider::config()
+		   * https://github.com/mage2pro/ach/blob/0.0.4/ConfigProvider.php#L19
+		   */
 		,'btCheckoutMessage' => $s->v('btCheckoutMessage')
 	] + self::configOptions($this) + parent::config();}
 }
